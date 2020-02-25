eedc378a8c839da1a4881d64a79f1f2cee2453c3
David <david@thejurne.com>
2020-02-25 13:31:49 -0800

    /api/notes appends request to db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
@@ -7,14 +8,22 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/api/tables', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
 
-app.post('/api/reserve', (req, res) => {
-  console.log(req.body);
-  res.send('Request posted, reservation made.');
+app.post('/api/notes', (req, res) => {
+  const apiPost = req.body;
+  fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+    let json = JSON.parse(data);
+    json.push(apiPost);
+    fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(json), err => {
+      if (err) throw err;
+      console.log('Appended notes to the database!');
+    });
+  });
+  res.send('DB has been modified.');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
