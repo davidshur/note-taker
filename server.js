38ba10770c8b89ed704ca12c1c6798c9cb2ce34e
David <david@thejurne.com>
2020-02-25 15:08:37 -0800

    use expresss static and public

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public/'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
